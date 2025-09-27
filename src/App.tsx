a9e5dca403663b865218c33eee8d16496d27bfc4
bvv-1 <53532178+bvv-1@users.noreply.github.com>
2025-09-27 14:40:21 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Title, Button } from '@mantine/core';
+
+function App() {
+  return (
+    <div className="container">
+      <Title>Tauri + React + Vite + Mantine</Title>
+      <Button>Hello Mantine!</Button>
+    </div>
+  );
+}
+
+export default App;
